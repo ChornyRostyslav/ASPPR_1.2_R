ba6d1249bf2b98ea7255c24613ee78fa165992af
barab <barab@UNEXI>
2025-04-03 01:44:14 +0400

    Добавьте файлы проекта.

---
+++ ASPPR_1.2_R/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace ASPPR_1._2_R
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textZ = new System.Windows.Forms.TextBox();
+            this.textOptimalSolution = new System.Windows.Forms.TextBox();
+            this.buttonSolve = new System.Windows.Forms.Button();
+            this.numVar = new System.Windows.Forms.NumericUpDown();
+            this.textConstraints = new System.Windows.Forms.TextBox();
+            this.textObjective = new System.Windows.Forms.TextBox();
+            this.textSupportSolution = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBoxSaveLog = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numVar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label2.Location = new System.Drawing.Point(144, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 24);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Обмеження:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label1.Location = new System.Drawing.Point(144, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 24);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Z:";
+            // 
+            // textZ
+            // 
+            this.textZ.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textZ.Location = new System.Drawing.Point(148, 394);
+            this.textZ.Name = "textZ";
+            this.textZ.Size = new System.Drawing.Size(239, 29);
+            this.textZ.TabIndex = 15;
+            // 
+            // textOptimalSolution
+            // 
+            this.textOptimalSolution.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textOptimalSolution.Location = new System.Drawing.Point(148, 278);
+            this.textOptimalSolution.Multiline = true;
+            this.textOptimalSolution.Name = "textOptimalSolution";
+            this.textOptimalSolution.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textOptimalSolution.Size = new System.Drawing.Size(239, 51);
+            this.textOptimalSolution.TabIndex = 14;
+            // 
+            // buttonSolve
+            // 
+            this.buttonSolve.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSolve.Location = new System.Drawing.Point(424, 126);
+            this.buttonSolve.Name = "buttonSolve";
+            this.buttonSolve.Size = new System.Drawing.Size(120, 41);
+            this.buttonSolve.TabIndex = 13;
+            this.buttonSolve.Text = "Обчислити";
+            this.buttonSolve.UseVisualStyleBackColor = true;
+            this.buttonSolve.Click += new System.EventHandler(this.buttonSolve_Click);
+            // 
+            // numVar
+            // 
+            this.numVar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numVar.Location = new System.Drawing.Point(424, 55);
+            this.numVar.Name = "numVar";
+            this.numVar.Size = new System.Drawing.Size(120, 29);
+            this.numVar.TabIndex = 12;
+            // 
+            // textConstraints
+            // 
+            this.textConstraints.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textConstraints.Location = new System.Drawing.Point(148, 126);
+            this.textConstraints.Multiline = true;
+            this.textConstraints.Name = "textConstraints";
+            this.textConstraints.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textConstraints.Size = new System.Drawing.Size(239, 104);
+            this.textConstraints.TabIndex = 11;
+            // 
+            // textObjective
+            // 
+            this.textObjective.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textObjective.Location = new System.Drawing.Point(148, 54);
+            this.textObjective.Name = "textObjective";
+            this.textObjective.Size = new System.Drawing.Size(239, 29);
+            this.textObjective.TabIndex = 10;
+            // 
+            // textSupportSolution
+            // 
+            this.textSupportSolution.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textSupportSolution.Location = new System.Drawing.Point(424, 278);
+            this.textSupportSolution.Multiline = true;
+            this.textSupportSolution.Name = "textSupportSolution";
+            this.textSupportSolution.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textSupportSolution.Size = new System.Drawing.Size(239, 51);
+            this.textSupportSolution.TabIndex = 20;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label3.Location = new System.Drawing.Point(144, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(231, 24);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Оптимальний розв\'язок:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label4.Location = new System.Drawing.Point(420, 251);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(188, 24);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Опорний розв\'язок:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label5.Location = new System.Drawing.Point(144, 367);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 24);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Max (Z):";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.label6.Location = new System.Drawing.Point(420, 27);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(171, 24);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "Кількість змінних:";
+            // 
+            // checkBoxSaveLog
+            // 
+            this.checkBoxSaveLog.AutoSize = true;
+            this.checkBoxSaveLog.Location = new System.Drawing.Point(424, 187);
+            this.checkBoxSaveLog.Name = "checkBoxSaveLog";
+            this.checkBoxSaveLog.Size = new System.Drawing.Size(110, 17);
+            this.checkBoxSaveLog.TabIndex = 25;
+            this.checkBoxSaveLog.Text = "Логувати у файл";
+            this.checkBoxSaveLog.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 450);
+            this.Controls.Add(this.checkBoxSaveLog);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textSupportSolution);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textZ);
+            this.Controls.Add(this.textOptimalSolution);
+            this.Controls.Add(this.buttonSolve);
+            this.Controls.Add(this.numVar);
+            this.Controls.Add(this.textConstraints);
+            this.Controls.Add(this.textObjective);
+            this.Name = "Form1";
+            this.Text = "Chonryi R.S. 631";
+            ((System.ComponentModel.ISupportInitialize)(this.numVar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textZ;
+        private System.Windows.Forms.TextBox textOptimalSolution;
+        private System.Windows.Forms.Button buttonSolve;
+        private System.Windows.Forms.NumericUpDown numVar;
+        private System.Windows.Forms.TextBox textConstraints;
+        private System.Windows.Forms.TextBox textObjective;
+        private System.Windows.Forms.TextBox textSupportSolution;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckBox checkBoxSaveLog;
+    }
+}
+
---
+++ ASPPR_1.2_R/Form1.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ASPPR_1._2_R
+{
+    public partial class Form1 : Form
+    {
+        private string logText = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double[,] ModifiedJordanElimination(double[,] matrix, int r, int s)
+        {
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            double[,] newMatrix = (double[,])matrix.Clone();
+            double originalPivot = matrix[r, s];
+
+            logText += $"Розв’язувальний елемент: [{r}, {s}] = {originalPivot:F2}\n";
+
+            newMatrix[r, s] = 1.0;
+
+            for (int i = 1; i < rows; i++)
+            {
+                if (i != r)
+                {
+                    newMatrix[i, s] = -newMatrix[i, s];
+                }
+            }
+
+            for (int i = 1; i < rows; i++)
+            {
+                if (i == r) continue;
+                for (int j = 1; j < cols; j++)
+                {
+                    if (j == s) continue;
+                    newMatrix[i, j] = (matrix[i, j] * originalPivot) - (matrix[i, s] * matrix[r, j]);
+                }
+            }
+
+            for (int i = 1; i < rows; i++)
+            {
+                for (int j = 1; j < cols; j++)
+                {
+                    newMatrix[i, j] /= originalPivot;
+                }
+            }
+
+            SwapElements(newMatrix, r, 0, 0, s);
+
+            logText += "Матриця після виконання МЖВ:\n" + MatrixToString(newMatrix);
+
+            return newMatrix;
+        }
+
+        public void SwapElements(double[,] array, int row1, int col1, int row2, int col2)
+        {
+            double temp = array[row1, col1];
+            array[row1, col1] = array[row2, col2];
+            array[row2, col2] = temp;
+        }
+        private string MatrixToString(double[,] matrix)
+        {
+            string result = "";
+            int rows = matrix.GetLength(0);
+            int cols = matrix.GetLength(1);
+            for (int i = 1; i < rows; i++)
+            {
+                for (int j = 1; j < cols; j++)
+                {
+                    result += matrix[i, j].ToString("F2") + " ";
+                }
+                result += "\n";
+            }
+            return result;
+        }
+
+        private string SolutionToString(double[] solution)
+        {
+            string result = "";
+            int cols = solution.GetLength(0);
+            for (int j = 0; j < cols; j++)
+            {
+                result += solution[j].ToString("F2") + " ";
+            }
+            result += "\n";
+            return result;
+        }
+
+        private void SolveLinearProgrammingProblem(double[] objectiveFunction, double[,] constraints, int[] constraintTypes)
+        {
+            int numConstraints = constraintTypes.Length;
+            int cols = constraints.GetLength(1);
+            int rows = constraints.GetLength(0);
+            int n = cols - 1;
+            double[] x = new double[(int)numVar.Value];
+            double[,] transformedConstraints = new double[numConstraints + 1, cols];
+            double[,] simplexTable = new double[numConstraints+2, cols+1];
+
+            logText = "Перепишемо систему обмежень:\n";
+
+            for (int i = 0; i < numConstraints; i++)
+            {
+                if (constraintTypes[i] == 1)
+                {
+                    transformedConstraints[i, n] = constraints[i, n];
+                    for (int k = 0; k < n; k++)
+                    {
+                        transformedConstraints[i, k] = constraints[i, k] * -1;
+                    }
+                }
+                else
+                {
+                    transformedConstraints[i, n] = -constraints[i, n];
+                    for (int k = 0; k < n; k++)
+                    {
+                        transformedConstraints[i, k] = constraints[i, k];
+                    }
+                }
+                for (int j = 0; j < n; j++)
+                {
+                    logText += $"{transformedConstraints[i, j]:F2} * X[{j+1}]";
+                    transformedConstraints[i, j] *= -1;
+                    if(j < n-1)
+                    {
+                        logText += " + ";
+                    }
+                    else
+                    {
+                        logText += $" + {transformedConstraints[i, cols-1]} >= 0\n";
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                transformedConstraints[numConstraints, i] = objectiveFunction[i] * -1;
+            }
+
+            for (int i = 0; i < cols + 1; i++)
+            {
+                simplexTable[0, i] = i;
+                if (i == cols)
+                {
+                    simplexTable[0, i] = 1;
+                }
+            }
+
+            for (int i = 0; i < numConstraints+2; i++)
+            {
+                simplexTable[i, 0] = -i;
+            }
+
+            for (int i = 1; i < numConstraints+2; i++)
+            {
+                for (int j = 1; j < cols+1; j++)
+                {
+                    simplexTable[i, j] = transformedConstraints[i-1, j-1];
+                }
+            }
+
+            (x, simplexTable) = FindSupportSolution(simplexTable);
+            logText += "Знайдено опорний розв'язок: " + SolutionToString(x);
+            textSupportSolution.Text = SolutionToString(x);
+
+            (x, simplexTable) = FindOptimalSolution(simplexTable);
+            logText += "Знайдено оптимальний розв'язок: " + SolutionToString(x);
+            textOptimalSolution.Text = SolutionToString(x);
+
+            logText += $"\nMax (Z) = {simplexTable[numConstraints + 1, cols]}";
+            textZ.Text = simplexTable[numConstraints + 1, cols].ToString("F2");
+
+            if (checkBoxSaveLog.Checked)
+            {
+                SaveLogToFile();
+            }
+        }
+
+        private (double[], double[,]) FindSupportSolution(double[,] simplexTable)
+        {
+            int rows = simplexTable.GetLength(0);
+            int cols = simplexTable.GetLength(1);
+            double[] x = new double[(int)numVar.Value];
+            double[,] newSimplexTable = (double[,])simplexTable.Clone();
+
+            int pivotRow;
+            int pivotCol;
+            double minRatio;
+
+            while (true)
+            {
+                pivotRow = -1;
+                pivotCol = -1;
+                simplexTable = (double[,])newSimplexTable.Clone();
+                minRatio = double.MaxValue;
+
+                for (int i = 1; i < rows; i++)
+                {
+                    if (simplexTable[i, cols - 1] < 0)
+                    {
+                        pivotRow = i;
+                        break;
+                    }
+                }
+
+                if (pivotRow == -1) break;
+
+                for (int i = 1; i < cols + 1; i++)
+                {
+                    if (simplexTable[pivotRow, i] < 0)
+                    {
+                        pivotCol = i;
+                        break;
+                    }
+                }
+
+                if (pivotCol == -1) break;
+
+                for (int i = 1; i < rows; i++)
+                {
+                    simplexTable[i, cols - 1] /= simplexTable[i, pivotCol];
+                }
+
+                for (int i = 1; i < rows; i++)
+                {
+                    if (simplexTable[i, cols - 1] < minRatio && simplexTable[i, cols - 1] > 0 && simplexTable[i,pivotCol] > 0)
+                    {
+                        minRatio = simplexTable[i, cols - 1];
+                        pivotRow = i;
+                    }
+                }
+
+                newSimplexTable = ModifiedJordanElimination(newSimplexTable, pivotRow, pivotCol);
+
+                for (int i = 1; i < rows; i++)
+                {
+                    if (newSimplexTable[i, 0] > 0)
+                    {
+                        x[(int)newSimplexTable[i, 0] - 1] = newSimplexTable[i, cols - 1];
+                    }
+                }
+            }
+            return (x, newSimplexTable);
+        }
+
+        private (double[], double[,]) FindOptimalSolution(double[,] simplexTable)
+        {
+            int rows = simplexTable.GetLength(0);
+            int cols = simplexTable.GetLength(1);
+            double[] x = new double[(int)numVar.Value];
+            double[,] newSimplexTable = (double[,])simplexTable.Clone();
+
+            int pivotRow;
+            int pivotCol;
+            double minRatio;
+            while (true)
+            {
+                pivotCol = -1;
+                pivotRow = -1;
+                simplexTable = (double[,])newSimplexTable.Clone();
+                minRatio = double.MaxValue;
+
+                for (int i = 1; i < cols; i++)
+                {
+                    if (simplexTable[rows-1, i] < 0)
+                    {
+                        if (pivotCol == -1)
+                        {
+                            pivotCol = i;
+                            break;
+                        }
+                    }
+
+                }
+
+                if (pivotCol == -1) break;
+
+                for (int i = 1; i < rows; i++)
+                {
+                    simplexTable[i, cols - 1] /= simplexTable[i, pivotCol];
+                }
+
+                for (int i = 1; i < rows; i++)
+                {
+                    if (simplexTable[i, cols - 1] < minRatio && simplexTable[i, cols - 1] >= 0 && simplexTable[i, pivotCol] > 0)
+                    {
+                        minRatio = simplexTable[i, cols - 1];
+                        pivotRow = i;
+                    }
+                }
+
+                newSimplexTable = ModifiedJordanElimination(newSimplexTable, pivotRow, pivotCol);
+
+                x = new double[(int)numVar.Value];
+
+                for(int i = 1; i < rows; i++)
+                {
+                    if (newSimplexTable[i, 0] > 0)
+                    {
+                        x[(int)newSimplexTable[i, 0] - 1] = newSimplexTable[i, cols - 1];
+                    }
+                }
+
+            }
+            return (x, newSimplexTable);
+        }
+
+        private void buttonSolve_Click(object sender, EventArgs e)
+        {
+            double[,] constraints = ParseConstraints(textConstraints.Text);
+            double[] objectiveFunction = ParseObjective(textObjective.Text);
+            int[] constraintTypes = ParseConstraintTypes(textConstraints.Text);
+            SolveLinearProgrammingProblem(objectiveFunction, constraints, constraintTypes);
+        }
+
+        private double[,] ParseConstraints(string input)
+        {
+            string[] lines = input.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            int numRows = lines.Length;
+            int numCols = 0;
+
+            foreach (var line in lines)
+            {
+                var matches = Regex.Matches(line, @"[+-]?\d*x\d+");
+                numCols = Math.Max(numCols, matches.Count);
+            }
+
+            double[,] matrix = new double[numRows, numCols + 1];
+
+            for (int i = 0; i < numRows; i++)
+            {
+                var matches = Regex.Matches(lines[i], @"([+-]?\d*)x(\d+)");
+                foreach (Match match in matches)
+                {
+                    int coefficient = match.Groups[1].Value == "-" ? -1 :
+                                      string.IsNullOrEmpty(match.Groups[1].Value) || match.Groups[1].Value == "+" ? 1 :
+                                      int.Parse(match.Groups[1].Value);
+                    int index = int.Parse(match.Groups[2].Value) - 1;
+                    matrix[i, index] = coefficient;
+                }
+
+                var rhsMatch = Regex.Match(lines[i], @"[<>]=\s*(-?\d+)");
+                if (rhsMatch.Success)
+                {
+                    matrix[i, numCols] = double.Parse(rhsMatch.Groups[1].Value);
+                }
+            }
+
+            return matrix;
+        }
+
+        private int[] ParseConstraintTypes(string input)
+        {
+            string[] lines = input.Split(new[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            int[] constraintTypes = new int[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                if (lines[i].Contains("<="))
+                {
+                    constraintTypes[i] = 1;
+                }
+                else if (lines[i].Contains(">="))
+                {
+                    constraintTypes[i] = -1;
+                }
+                else if (lines[i].Contains("="))
+                {
+                    constraintTypes[i] = 0;
+                }
+                else
+                {
+                    throw new ArgumentException("Невірний формат обмежень");
+                }
+            }
+
+            return constraintTypes;
+        }
+
+        private double[] ParseObjective(string input)
+        {
+            var matches = Regex.Matches(input, @"([+-]?\d*)x(\d+)");
+            int numVars = (int)numVar.Value;
+
+            double[] coefficients = new double[numVars];
+
+            foreach (Match match in matches)
+            {
+                int coefficient = match.Groups[1].Value == "-" ? -1 :
+                                  string.IsNullOrEmpty(match.Groups[1].Value) || match.Groups[1].Value == "+" ? 1 :
+                                  int.Parse(match.Groups[1].Value);
+                int index = int.Parse(match.Groups[2].Value) - 1;
+                coefficients[index] = coefficient;
+            }
+
+            return coefficients;
+        }
+
+        private void SaveLogToFile()
+        {
+            using (FolderBrowserDialog folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = Path.Combine(folderDialog.SelectedPath, "log.txt");
+                    File.WriteAllText(filePath, logText);
+                    MessageBox.Show("Лог збережено: " + filePath, "Збереження логу", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+    }
+}
